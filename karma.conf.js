6b4b9fbe9a99b65d61c81e841e96ec72a3528afb
Timothy Ruhle <timothy.ruhle@intellipharm.com.au>
2015-04-17 08:30:17 +1000

    Still trying to get travis to work

--- karma.conf.js
+++ karma.conf.js
@@ -2,76 +2,76 @@
 // Generated on Tue Jan 27 2015 07:58:14 GMT+1000 (EST)
 
 module.exports = function(config) {
-	config.set({
+    config.set({
 
-		// base path that will be used to resolve all patterns (eg. files, exclude)
-		basePath: '',
-
-
-		// frameworks to use
-		// available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-		frameworks: ['jasmine'],
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: '',
 
 
-		// list of files / patterns to load in the browser
-		files: [
-			'bower_components/lodash/lodash.min.js',
-			'bower_components/angular/angular.min.js',
-			'bower_components/angular-mocks/angular-mocks.js',
-			'bower_components/pluralize/pluralize.js',
-			'lib/angular-cakephp.js',
-			'lib/angular-cakephp-settings.js',
-			'lib/http/http-response-service.js',
-			'lib/http/http-request-service.js',
-			'lib/transformers/transformer-service.js',
-			'lib/util/util-service.js',
-			'lib/base/base-active-record.js',
-			'lib/base/base-model.js',
-			'tests/*.js'
-		],
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['jasmine'],
 
 
-		// list of files to exclude
-		exclude: [
-		],
+        // list of files / patterns to load in the browser
+        files: [
+            'bower_components/lodash/lodash.min.js',
+            'bower_components/angular/angular.min.js',
+            'bower_components/angular-mocks/angular-mocks.js',
+            'bower_components/pluralize/pluralize.js',
+            'lib/angular-cakephp.js',
+            'lib/angular-cakephp-settings.js',
+            'lib/http/http-response-service.js',
+            'lib/http/http-request-service.js',
+            'lib/transformers/transformer-service.js',
+            'lib/util/util-service.js',
+            'lib/base/base-active-record.js',
+            'lib/base/base-model.js',
+            'tests/*.js'
+        ],
 
 
-		// preprocess matching files before serving them to the browser
-		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-		preprocessors: {
-		},
+        // list of files to exclude
+        exclude: [
+        ],
 
 
-		// test results reporter to use
-		// possible values: 'dots', 'progress'
-		// available reporters: https://npmjs.org/browse/keyword/karma-reporter
-		reporters: ['progress'],
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {
+        },
 
 
-		// web server port
-		port: 9876,
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ['progress'],
 
 
-		// enable / disable colors in the output (reporters and logs)
-		colors: true,
+        // web server port
+        port: 9876,
 
 
-		// level of logging
-		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-		logLevel: config.LOG_INFO,
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
 
-		// enable / disable watching file and executing tests whenever any file changes
-		autoWatch: true,
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
 
-		// start these browsers
-		// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-		browsers: ['Chrome'],
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
 
 
-		// Continuous Integration mode
-		// if true, Karma captures browsers, runs the tests and exits
-		singleRun: false
-	});
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        browsers: ['Chrome', 'Firefox'],
+
+
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: false
+    });
 };
