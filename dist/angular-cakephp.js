8b6ea78716e102ca1d5f42d1e6db9f16301e0d5e
Timothy Ruhle <timothy.ruhle@intellipharm.com.au>
2015-11-24 14:06:32 +1000

    Adding in a fix for IE

--- dist/angular-cakephp.js
+++ dist/angular-cakephp.js
@@ -1,4 +1,6 @@
 System.register(['./rest-api/rest-api', './base-model/base-model', './active-record/active-record'], function (_export) {
+
+    // IE Hack
     'use strict';
 
     var RestApi, BaseModel, ActiveRecord;
@@ -11,6 +13,17 @@ System.register(['./rest-api/rest-api', './base-model/base-model', './active-rec
             ActiveRecord = _activeRecordActiveRecord['default'];
         }],
         execute: function () {
+            if (Function.prototype.name === undefined && Object.defineProperty !== undefined) {
+                Object.defineProperty(Function.prototype, 'name', {
+                    get: function get() {
+                        var funcNameRegex = /function\s([^(]{1,})\(/;
+                        var results = funcNameRegex.exec(this.toString());
+                        return results && results.length > 1 ? results[1].trim() : "";
+                    },
+                    set: function set(value) {}
+                });
+            }
+
             _export('RestApi', RestApi);
 
             _export('BaseModel', BaseModel);
--- src/angular-cakephp.js
+++ src/angular-cakephp.js
@@ -2,6 +2,18 @@ import RestApi from './rest-api/rest-api';
 import BaseModel from './base-model/base-model';
 import ActiveRecord from './active-record/active-record';
 
+// IE Hack
+if (Function.prototype.name === undefined && Object.defineProperty !== undefined) {
+    Object.defineProperty(Function.prototype, 'name', {
+        get: function() {
+            var funcNameRegex = /function\s([^(]{1,})\(/;
+            var results = (funcNameRegex).exec((this).toString());
+            return (results && results.length > 1) ? results[1].trim() : "";
+        },
+        set: function(value) {}
+    });
+}
+
 export {
     RestApi as RestApi,
     BaseModel as BaseModel,
